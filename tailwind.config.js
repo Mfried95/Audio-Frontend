c53775b4aec15d9853a3aa5367153afb2e89215d
mfried95 <m.friedman1995@gmail.com>
2024-05-02 18:17:07 -0400

    Installed Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // adjust this line based on your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
