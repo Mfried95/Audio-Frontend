446928440319ab789bfaba73ba09bdca8c41c038
mfried95 <m.friedman1995@gmail.com>
2024-05-03 18:58:14 -0400

    Created base pages and componenets

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import React, { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 const Home = lazy(() => import("../src/pages/Home"));
+const Natigation = lazy(() => import("../src/components/Navigation"));
+
 
 function App() {
+
   return (
     <div>
       <Suspense fallback={<div>Loading...</div>}>  {/* Provide a fallback while the component is loading */}
+      <Natigation/>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
