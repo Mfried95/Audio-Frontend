255294058130a6d65b95207c4a0c9552bc94e9f6
mfried95 <m.friedman1995@gmail.com>
2024-05-16 18:37:45 -0400

    Created Product component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HomeHeader from '../components/Home/HomeHeader.jsx'
+import Products from '../components/Home/Products.jsx'
 
 const Home = () => {
   return (
     <div>
       <HomeHeader/>
+      <Products/>
     </div>
   )
 }
