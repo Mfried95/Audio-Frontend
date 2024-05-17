d8d1614b0b583d597add38f6a6567103a6a259d3
mfried95 <m.friedman1995@gmail.com>
2024-05-17 15:14:53 -0400

    - Completed Tablet and Mobile view

--- frontend/src/components/Home/Products.jsx
+++ frontend/src/components/Home/Products.jsx
@@ -1,32 +1,38 @@
 import React from "react";
 import ShopIcon from "../../assets/shop-icon.svg";
-import Headphones from "../../assets/home/home-preview-headphones.svg"
-import Speakers from "../../assets/home/home-preview-speakers.svg"
-import Earphones from "../../assets/home/home-preview-earphones.svg"
+import Headphones from "../../assets/home/home-preview-headphones.svg";
+import Speakers from "../../assets/home/home-preview-speakers.svg";
+import Earphones from "../../assets/home/home-preview-earphones.svg";
 
 const Products = () => {
   return (
-    <div className="flex gap-5 justify-center mt-[10em] h-screen">
-      <div className="bg-[#F1F1F1] h-[200px] w-[22%] rounded-lg flex flex-col items-center justify-center ">
-        <img src={Headphones} alt="" className="absolute mb-[15em]" />
-        <h2 className="font-manrope font-bold text-lg tracking-wide mt-8">HEADPHONES</h2>
-        <button className="flex items-center mt-2 text-gray-500">
+    <div className="flex flex-col md:flex-row gap-5 justify-center mt-[10em] min-h-screen px-5">
+      <div className="bg-[#F1F1F1] h-[200px] w-full md:w-[45%] lg:w-[22%] rounded-lg flex flex-col items-center justify-center relative mb-[6em] md:mb-0">
+        <img src={Headphones} alt="Headphones" className="absolute mb-[15em]" />
+        <h2 className="font-manrope font-bold text-lg tracking-wide mt-8">
+          HEADPHONES
+        </h2>
+        <button className="flex items-center mt-2 text-gray-500 hover:text-nav-hover">
           SHOP
           <img src={ShopIcon} alt="Shop Icon" className="ml-2" />
         </button>
       </div>
-      <div className="bg-[#F1F1F1] h-[200px] w-[22%] rounded-lg flex flex-col items-center justify-center ">
-        <img src={Speakers} alt="" className="absolute mb-[15em]" />
-        <h2 className="font-manrope font-bold text-lg tracking-wide mt-8">SPEAKERS</h2>
-        <button className="flex items-center mt-2 text-gray-500">
+      <div className="bg-[#F1F1F1] h-[200px] w-full md:w-[45%] lg:w-[22%] rounded-lg flex flex-col items-center justify-center relative mb-[6em] md:mb-0">
+        <img src={Speakers} alt="Speakers" className="absolute mb-[15em]" />
+        <h2 className="font-manrope font-bold text-lg tracking-wide mt-8">
+          SPEAKERS
+        </h2>
+        <button className="flex items-center mt-2 text-gray-500 hover:text-nav-hover">
           SHOP
           <img src={ShopIcon} alt="Shop Icon" className="ml-2" />
         </button>
       </div>
-      <div className="bg-[#F1F1F1] h-[200px] w-[22%] rounded-lg flex flex-col items-center justify-center ">
-        <img src={Earphones} alt="" className="absolute mb-[15em]" />
-        <h2 className="font-manrope font-bold text-lg tracking-wide mt-8">EARPHONES</h2>
-        <button className="flex items-center mt-2 text-gray-500">
+      <div className="bg-[#F1F1F1] h-[200px] w-full md:w-[45%] lg:w-[22%] rounded-lg flex flex-col items-center justify-center relative mb-10 md:mb-0">
+        <img src={Earphones} alt="Earphones" className="absolute mb-[15em]" />
+        <h2 className="font-manrope font-bold text-lg tracking-wide mt-8">
+          EARPHONES
+        </h2>
+        <button className="flex items-center mt-2 text-gray-500 hover:text-nav-hover">
           SHOP
           <img src={ShopIcon} alt="Shop Icon" className="ml-2" />
         </button>
