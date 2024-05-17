535d60ef7f9c0bba6ef8cf9238e0e107112b87f7
mfried95 <m.friedman1995@gmail.com>
2024-05-17 13:58:07 -0400

    Installed CORS

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors'); // Importing cors package
 const app = express();
 const PORT = process.env.PORT || 3000;
 const productRoutes = require('./routes/product'); // Ensure the correct path
 
+app.use(cors()); // Enable CORS for all routes
 app.use(express.json());
 
 app.use('/products', productRoutes); // Mounting the routes
